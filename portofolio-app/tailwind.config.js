be264b5a1d7f688b8170e9ad9c27d7565158c907
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-03-02 16:21:53 +0700

    initial commit

--- portofolio-app/tailwind.config.js
+++ portofolio-app/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			fontFamily: {
+				sans: ["var(--font-mont)"],
+			},
 		},
 	},
 	plugins: [],
