a242f3343b69d0fbdfbb7aff349073170497b69a
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-03-03 18:22:26 +0700

    initial commit

--- portofolio-app/src/components/Logo.js
+++ portofolio-app/src/components/Logo.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const Logo = () => {
 	return (
 		<>
-			<div className="flex items-center justify-center mt-2">Logo</div>
+			<div className="flex items-center justify-center mt-2">
+				<Link href={"/"} className="w-16 h-16 bg-dark text-light items-center justify-center rounded-full">
+					3D
+				</Link>
+			</div>
 		</>
 	);
 };
