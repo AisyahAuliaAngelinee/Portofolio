514abe6807798502100f0704662903f1525ecc7d
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-03-03 19:11:30 +0700

    initial commit

--- portofolio-app/src/components/Logo.js
+++ portofolio-app/src/components/Logo.js
@@ -1,13 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { motion } from "framer-motion";
+
+const MotionLink = motion(Link);
 
 const Logo = () => {
 	return (
 		<>
 			<div className="flex items-center justify-center mt-2">
-				<Link href={"/"} className="w-16 h-16 bg-dark text-light items-center justify-center rounded-full">
+				<MotionLink
+					href={"/"}
+					className="w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold"
+					whileHover={{
+						scale: 1.2,
+					}}
+				>
 					3D
-				</Link>
+				</MotionLink>
 			</div>
 		</>
 	);
