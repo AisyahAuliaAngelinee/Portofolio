8929f842928f4c22093cd641ca3252b7c7a1b1f8
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-03-04 23:18:10 +0700

    initial commit

--- portofolio-app/src/app/page.js
+++ portofolio-app/src/app/page.js
@@ -1,9 +1,19 @@
+import Layout from "@/components/Layout";
 import Image from "next/image";
+import profilePic from "../../public/profile-picture.png";
 
 export default function Home() {
 	return (
 		<>
-			<main>Home</main>
+			<main className="flex items-center text-dark w-full min-h-screen">
+				<Layout>
+					<div className="flex items-center justify-between w-full">
+						<div>
+							<Image src={profilePic} alt="Vincentius Clarishna" className="w-full h-auto" />
+						</div>
+					</div>
+				</Layout>
+			</main>
 		</>
 	);
 }
