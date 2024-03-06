c8b4465aca11ddbb6b46b7a083ccf7210ac8feb8
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-03-06 23:12:58 +0700

    initial commit

--- portofolio-app/src/app/page.js
+++ portofolio-app/src/app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import profilePic from "../../public/profile-picture(1).png";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
+import { LinkArrow } from "@/components/Icons";
 
 export default function Home() {
 	return (
@@ -16,8 +17,13 @@ export default function Home() {
 						<div className="w-1/2 flex flex-col items-center self-center">
 							<AnimatedText text="Web Enthusiast, Design and Code!" className="!text-6xl text-left" />
 							<p className="my-4 text-base font-medium">As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. Explore my latest projects and articles, showcasing my expertise in React.js, Next.js and web development.</p>
-							<div>
-								<Link href={"/CV-Vincentius Clarishna-Full Version.pdf"}></Link>
+							<div className="flex items-center self-start mt-2">
+								<Link href={"/CV-Vincentius Clarishna-Full Version.pdf"} target={"_blank"} className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark" download={true}>
+									Resume <LinkArrow className={"w-6 ml-1"} />
+								</Link>
+								<Link href={"mailto:vincentius.clarishna@gmail.com"} target={"_blank"} className="ml-4 text-lg font-medium capitalize text-dark underline">
+									Contact
+								</Link>
 							</div>
 						</div>
 					</div>
