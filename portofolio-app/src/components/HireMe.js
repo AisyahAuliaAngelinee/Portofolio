09c23d2e0559266ee0c0a7d569fe4a6f3e0fc59b
Aisyah Aulia Angelinee <vincentius.clarishna@gmail.com>
2024-03-07 10:37:24 +0700

    initial commit

--- portofolio-app/src/components/HireMe.js
+++ portofolio-app/src/components/HireMe.js
@@ -6,7 +6,7 @@ const HireMe = () => {
 		<>
 			<div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden">
 				<div className="w-48 h-auto flex items-center justify-center relative">
-					<CircularText className={"fill-dark"} />
+					<CircularText className={"fill-dark animate-spin"} />
 				</div>
 			</div>
 		</>
