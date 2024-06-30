e9cbc272280a37eaf0a51d3156dccd3ca4a76f8a
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2024-07-01 00:11:07 +0700

    feat: homepage

--- personal-website/src/components/Stats.tsx
+++ personal-website/src/components/Stats.tsx
@@ -29,7 +29,7 @@ interface statsProp {
 const Stats = () => {
 	return (
 		<>
-			<section>
+			<section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
 				<div className="container mx-auto">
 					<div className="flex flex-wrap gap-6 max-w-[80dvw] mx-auto xl:max-w-none">
 						{stats.map((item: statsProp, index: number) => {
@@ -37,7 +37,7 @@ const Stats = () => {
 								<>
 									<div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
 										<CountUp end={item.number} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold" />
-										<p>{item.text}</p>
+										<p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
 									</div>
 								</>
 							);
