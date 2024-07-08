101450452c4391f414f96a395a6a5a1c59eb2cde
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2024-07-08 10:07:44 +0700

    feat: service page

--- personal-website/src/app/services/page.tsx
+++ personal-website/src/app/services/page.tsx
@@ -1,6 +1,16 @@
 "use client";
 
 import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+const services = [
+	{
+		id: 1,
+		title: "Web Developement",
+		description:
+			"A web developer must be proficient in various programming languages such as HTML, CSS, and JavaScript, as well as understanding responsiveness and accessibility to create attractive and functional websites.",
+	},
+];
 
 function Services() {
 	return (
