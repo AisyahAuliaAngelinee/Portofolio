f7411517b49493a64ab2b5398b513b73f75accfd
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2024-07-12 23:05:44 +0700

    feat: service

--- personal-website/src/app/services/page.tsx
+++ personal-website/src/app/services/page.tsx
@@ -5,10 +5,25 @@ import Link from "next/link";
 
 const services = [
 	{
-		id: 1,
+		num: "01",
 		title: "Web Developement",
 		description:
 			"A web developer must be proficient in various programming languages such as HTML, CSS, and JavaScript, as well as understanding responsiveness and accessibility to create attractive and functional websites.",
+		href: "",
+	},
+	{
+		num: "02",
+		title: "Frontend Development",
+		description:
+			"A web developer must be proficient in various programming languages such as HTML, CSS, and JavaScript, as well as understanding responsiveness and accessibility to create attractive and functional websites.",
+		href: "",
+	},
+	{
+		num: "03",
+		title: "Fullstack Development",
+		description:
+			"A web developer must be proficient in various programming languages such as HTML, CSS, and JavaScript, as well as understanding responsiveness and accessibility to create attractive and functional websites.",
+		href: "",
 	},
 ];
 
