5e5a5099ddb60f735c6436324ce6673e5e780845
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2024-07-13 23:15:52 +0700

    feat: resume experience

--- personal-website/src/app/resume/page.tsx
+++ personal-website/src/app/resume/page.tsx
@@ -5,6 +5,8 @@ import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
 import { SiTailwindcss, SiNextdotjs, SiExpress, SiSequelize, SiPostgresql, SiTypescript } from "react-icons/si";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
 
 interface aboutProps {
 	fieldName: string;
@@ -167,7 +169,53 @@ const skills = {
 const Resume = () => {
 	return (
 		<>
-			<div>resume page</div>
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+				className="min-h-[80dvh] flex items-center justify-center py-12 xl:py-0">
+				<div className="container mx-auto">
+					<Tabs defaultValue="experience" className="flex flex-col xl:flex-row gap-[60px]">
+						<TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+							<TabsTrigger value="experience">Experience</TabsTrigger>
+							<TabsTrigger value="education">Education</TabsTrigger>
+							<TabsTrigger value="skills">Skills</TabsTrigger>
+							<TabsTrigger value="about">About Me</TabsTrigger>
+						</TabsList>
+
+						<div className="min-h-[70dvh] w-full">
+							<TabsContent value="experience" className="w-full">
+								<div className="flex flex-col gap-[30px] text-center xl:text-left">
+									<h3 className="text-4xl font-bold">{experience.title}</h3>
+									<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
+									<ScrollArea className="h-[400px]">
+										<ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+											{experience.items.map((dt: experienceProps, index: number) => (
+												<li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
+													<span>{dt.duration}</span>
+													<h3>{dt.position}</h3>
+													<div>
+														<span></span>
+														<p>{dt.company}</p>
+													</div>
+												</li>
+											))}
+										</ul>
+									</ScrollArea>
+								</div>
+							</TabsContent>
+							<TabsContent value="education" className="w-full">
+								education
+							</TabsContent>
+							<TabsContent value="skills" className="w-full">
+								skills
+							</TabsContent>
+							<TabsContent value="about" className="w-full">
+								about me
+							</TabsContent>
+						</div>
+					</Tabs>
+				</div>
+			</motion.div>
 		</>
 	);
 };
