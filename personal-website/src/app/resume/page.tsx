a945d87421502d07b4bf4f8e74154f4442bac71f
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2024-07-13 22:36:14 +0700

    feat: resume

--- personal-website/src/app/resume/page.tsx
+++ personal-website/src/app/resume/page.tsx
@@ -1,18 +1,31 @@
 "use client";
 
-import { Badge } from "@/assets";
+import { Badge, Cap, Golang } from "@/assets";
 import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
 import { SiTailwindcss, SiNextdotjs, SiExpress, SiSequelize, SiPostgresql, SiTypescript } from "react-icons/si";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
-interface infoProps {
+interface aboutProps {
 	fieldName: string;
 	fieldValue: string;
 }
 
-interface itemProps {
+interface experienceProps {
 	company: string;
 	position: string;
 	duration: string;
+}
+
+interface educationProps {
+	institution: string;
+	degree: string;
+	duration: string;
+}
+
+interface iconProps {
+	icon: any;
+	name: string;
 }
 
 const about = {
@@ -54,7 +67,7 @@ const about = {
 
 const experience = {
 	icon: Badge,
-	title: "My Experience",
+	title: "My experience",
 	description:
 		"I have one year of internship experience as a System Analyst and have since built my career as a Frontend Developer. My journey in frontend development continues to grow and evolve.",
 	items: [
@@ -67,6 +80,86 @@ const experience = {
 			company: "Kimberly-Clark Softex (PT. Softex Indonesia)",
 			position: "System Analyst Intern",
 			duration: "July 2022 - July 2023",
+		},
+	],
+};
+
+const education = {
+	icon: Cap,
+	title: "My education",
+	description:
+		"I graduated with a Bachelor's degree in Information Systems from Multimedia Nusantara University and furthered my education at Hacktiv8 Bootcamp, specializing in Fullstack JavaScript. This training has equipped me to pursue a career as a web developer.",
+	items: [
+		{
+			institution: "Bootcamp Hacktiv8 Online Course",
+			degree: "Golang Backend Developer",
+			duration: "March 2024",
+		},
+		{
+			institution: "Bootcamp Hacktiv8 Offline Course",
+			degree: "Fullstack Javascript Immersive",
+			duration: "September 2023 - January 2024",
+		},
+		{
+			institution: "Multimedia Nusantara University",
+			degree: "Information Systems",
+			duration: "July 2018 - July 2023",
+		},
+	],
+};
+
+const skills = {
+	title: "My skills",
+	description:
+		"I have skills in web programming, specializing in frontend development, and also possess basic skills in backend development. This combination allows me to create comprehensive and dynamic web applications.",
+	skillList: [
+		{
+			icon: <FaHtml5 />,
+			name: "html",
+		},
+		{
+			icon: <FaCss3 />,
+			name: "css",
+		},
+		{
+			icon: <FaJs />,
+			name: "javascript",
+		},
+		{
+			icon: <SiTypescript />,
+			name: "typescript",
+		},
+		{
+			icon: <FaReact />,
+			name: "react.js",
+		},
+		{
+			icon: <SiNextdotjs />,
+			name: "next.js",
+		},
+		{
+			icon: <SiTailwindcss />,
+			name: "tailwind.css",
+		},
+		{
+			icon: <FaNodeJs />,
+			name: "node.js",
+		},
+		{
+			icon: <SiExpress />,
+			name: "express.js",
+		},
+		{
+			icon: <SiSequelize />,
+			name: "sequelize",
+		},
+		{
+			icon: <SiPostgresql />,
+			name: "postgreSQL",
+		},
+		{
+			icon: Golang,
+			name: "golang",
 		},
 	],
 };
--- personal-website/src/assets/index.ts
+++ personal-website/src/assets/index.ts
@@ -1,4 +1,6 @@
 import ProfilePict from "./pp.png";
 import Badge from "./resume/badge.svg";
+import Cap from "./resume/cap.svg";
+import Golang from "./skills/Go-Logo_LightBlue.svg";
 
-export { ProfilePict, Badge };
+export { ProfilePict, Badge, Cap, Golang };
