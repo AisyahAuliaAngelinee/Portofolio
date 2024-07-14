c2ae7e2c6abed270af7803259affc19534aa47b0
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2024-07-14 15:19:24 +0700

    feat: button slider

--- personal-website/src/app/work/page.tsx
+++ personal-website/src/app/work/page.tsx
@@ -11,6 +11,8 @@ import Image from "next/image";
 import { FaCss3, FaJs, FaReact } from "react-icons/fa";
 import { SiTailwindcss, SiNextdotjs, SiTypescript, SiShadcnui } from "react-icons/si";
 import { TbBrandThreejs, TbBrandFramerMotion } from "react-icons/tb";
+import { Thumb1, Thumb2 } from "@/assets";
+import WorkSliderBtn from "@/components/WorkSliderBtn";
 
 interface stackProps {
 	icon: any;
@@ -23,7 +25,7 @@ interface projectProps {
 	title: string;
 	description: string;
 	stack: stackProps[];
-	image: string;
+	image: any;
 	link: string;
 	github: string;
 }
@@ -53,7 +55,7 @@ const projects = [
 				name: "tailwind.css",
 			},
 		],
-		image: "/assets/work/thumb1.png",
+		image: Thumb1,
 		link: "https://3-d-product-base.vercel.app/",
 		github: "https://github.com/AisyahAuliaAngelinee/3D-Shirt-Customizer",
 	},
@@ -89,7 +91,7 @@ const projects = [
 				name: "typescript",
 			},
 		],
-		image: "/assets/work/thumb2.png",
+		image: Thumb2,
 		link: "",
 		github: "",
 	},
@@ -98,19 +100,34 @@ const projects = [
 const Work = () => {
 	const [project, setProject] = useState<projectProps>(projects[0]);
 
+	const handleSlideChange = (swiper: any) => {
+		const currentIndex = swiper.activeIndex;
+		setProject(projects[currentIndex]);
+	};
+
 	return (
 		<>
-			<motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="min-h-[80dvh] flex flex-col justify-center py-12 xl:py-0">
+			<motion.section
+				initial={{ opacity: 0 }}
+				animate={{
+					opacity: 1,
+					transition: {
+						delay: 2.4,
+						duration: 0.4,
+						ease: "easeIn",
+					},
+				}}
+				className="min-h-[80dvh] flex flex-col justify-center py-12 xl:py-0">
 				<div className="container mx-auto">
 					<div className="flex flex-col xl:flex-row xl:gap-[30px]">
 						<div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-							<div>
+							<div className="flex flex-col gap-[30px] h-[50%]">
 								<div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
 								<h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 ease-in-out capitalize">
 									{project.category} project
 								</h2>
 								<p className="text-white/60">{project.description}</p>
-								<ul className="flex gap-4 pb-2">
+								<ul className="flex gap-4">
 									{project.stack.map((dt: stackProps, index: number) => (
 										<li key={index} className="text-xl text-accent">
 											<TooltipProvider delayDuration={100}>
@@ -127,7 +144,7 @@ const Work = () => {
 									))}
 								</ul>
 								<div className="border border-white/20"></div>
-								<div className="flex items-center gap-4 pt-2">
+								<div className="flex items-center gap-4">
 									<Link href={project.link} target="_blank">
 										<TooltipProvider delayDuration={100}>
 											<Tooltip>
@@ -155,7 +172,21 @@ const Work = () => {
 								</div>
 							</div>
 						</div>
-						<div className="w-full xl:w-[50%]">slider</div>
+						<div className="w-full xl:w-[50%]">
+							<Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
+								{projects.map((dt: projectProps, index: number) => (
+									<SwiperSlide key={index} className="w-full">
+										<div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+											<div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+											<div className="relative w-full h-full">
+												<Image src={dt.image} alt="project-thumbnail" fill className="object-cover" />
+											</div>
+										</div>
+									</SwiperSlide>
+								))}
+								<WorkSliderBtn />
+							</Swiper>
+						</div>
 					</div>
 				</div>
 			</motion.section>
--- personal-website/src/assets/index.ts
+++ personal-website/src/assets/index.ts
@@ -2,5 +2,7 @@ import ProfilePict from "./pp.png";
 import Badge from "./resume/badge.svg";
 import Cap from "./resume/cap.svg";
 import Golang from "./skills/Go-Logo_LightBlue.svg";
+import Thumb1 from "@/assets/work/thumb1.png";
+import Thumb2 from "@/assets/work/thumb2.png";
 
-export { ProfilePict, Badge, Cap, Golang };
+export { ProfilePict, Badge, Cap, Golang, Thumb1, Thumb2 };
