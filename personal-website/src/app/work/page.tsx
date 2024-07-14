95bf3f524dea384d72a1b1cae7480ec791276386
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2024-07-14 13:56:40 +0700

    feat: work

--- personal-website/src/app/work/page.tsx
+++ personal-website/src/app/work/page.tsx
@@ -8,10 +8,9 @@ import { BsArrowUpRight, BsGithub } from "react-icons/bs";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import Link from "next/link";
 import Image from "next/image";
-import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from "react-icons/fa";
-import { SiTailwindcss, SiNextdotjs, SiExpress, SiSequelize, SiPostgresql, SiTypescript } from "react-icons/si";
-import { BiLogoGoLang } from "react-icons/bi";
-import { TbBrandThreejs } from "react-icons/tb";
+import { FaCss3, FaJs, FaReact } from "react-icons/fa";
+import { SiTailwindcss, SiNextdotjs, SiTypescript, SiShadcnui } from "react-icons/si";
+import { TbBrandThreejs, TbBrandFramerMotion } from "react-icons/tb";
 
 interface stackProps {
 	icon: any;
@@ -21,34 +20,6 @@ interface stackProps {
 const projects = [
 	{
 		num: "01",
-		category: "frontend",
-		title: "Portofolio Website",
-		description:
-			"My portfolio website introduces myself and showcases the web projects I have completed, along with my skills in web programming. It also highlights the educational background and training I have undergone before starting my career.",
-		stack: [
-			{
-				icon: <TbBrandThreejs />,
-				name: "three.js",
-			},
-			{
-				icon: <FaJs />,
-				name: "javascript",
-			},
-			{
-				icon: <FaReact />,
-				name: "react.js",
-			},
-			{
-				icon: <SiTailwindcss />,
-				name: "tailwind.css",
-			},
-		],
-		image: "/assets/work/thumb1.png",
-		link: "",
-		github: "",
-	},
-	{
-		num: "02",
 		category: "frontend",
 		title: "3D Shirt Customizer",
 		description:
@@ -71,9 +42,45 @@ const projects = [
 				name: "tailwind.css",
 			},
 		],
-		image: "/assets/work/thumb2.png",
+		image: "/assets/work/thumb1.png",
 		link: "https://3-d-product-base.vercel.app/",
 		github: "https://github.com/AisyahAuliaAngelinee/3D-Shirt-Customizer",
+	},
+	{
+		num: "02",
+		category: "frontend",
+		title: "Personal Portofolio Website",
+		description:
+			"My portfolio website introduces myself and showcases the web projects I have completed, along with my skills in web programming. It also highlights the educational background and training I have undergone before starting my career.",
+		stack: [
+			{
+				icon: <SiNextdotjs />,
+				name: "next.js",
+			},
+			{
+				icon: <SiTailwindcss />,
+				name: "tailwind.css",
+			},
+			{
+				icon: <FaCss3 />,
+				name: "css",
+			},
+			{
+				icon: <TbBrandFramerMotion />,
+				name: "framer-motion",
+			},
+			{
+				icon: <SiShadcnui />,
+				name: "shadcn ui",
+			},
+			{
+				icon: <SiTypescript />,
+				name: "typescript",
+			},
+		],
+		image: "/assets/work/thumb2.png",
+		link: "",
+		github: "",
 	},
 ];
 
