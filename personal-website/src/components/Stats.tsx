d956ea6fcf60b7254fed9096257d0d55dce89710
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2026-03-24 02:43:28 +0700

    update: stats

--- personal-website/src/components/Stats.tsx
+++ personal-website/src/components/Stats.tsx
@@ -8,7 +8,7 @@ const stats = [
 		text: "Month of experience",
 	},
 	{
-		number: 2,
+		number: 3,
 		text: "Personal Projects Completed",
 	},
 	{
@@ -35,9 +35,19 @@ const Stats = () => {
 						{stats.map((item: statsProp, index: number) => {
 							return (
 								<>
-									<div key={index} className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
-										<CountUp end={item.number} duration={5} delay={2} className="text-4xl xl:text-6xl font-extrabold" />
-										<p className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>{item.text}</p>
+									<div
+										key={index}
+										className="flex-1 flex gap-4 items-center justify-center xl:justify-start">
+										<CountUp
+											end={item.number}
+											duration={5}
+											delay={2}
+											className="text-4xl xl:text-6xl font-extrabold"
+										/>
+										<p
+											className={`${item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80`}>
+											{item.text}
+										</p>
 									</div>
 								</>
 							);
