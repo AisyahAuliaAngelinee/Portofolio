28dbc05fff4765315c201e9c191ef83a2fe0a4d2
アンジェリン <vincentiusclarishna@Vincentiuss-MacBook-Pro.local>
2026-07-28 20:37:55 +0700

    fix: download-cv-link

--- personal-website/src/app/page.tsx
+++ personal-website/src/app/page.tsx
@@ -11,7 +11,7 @@ import Stats from "@/components/Stats";
 const Home = () => {
 	const handleDownloadPdf = () => {
 		window.open(
-			"https://drive.google.com/file/d/1OQRBGzpqQkq1sD1nO3OOJTYljc36R8BE/view?usp=sharing",
+			"https://drive.google.com/file/d/1QLMXQm-uWg_ynZYRvb_Uoc3foH8KsSKl/view?usp=sharing",
 			"_blank",
 			"noopener noreferrer",
 		);
